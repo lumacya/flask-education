f5657efd80729fe0a5d5d5d814f6e29f60a7f066
Ivan Gibert <vngbrt@gmail.com>
2023-02-07 18:16:05 +0300

    variable url

--- main.py
+++ main.py
@@ -2,13 +2,13 @@ from flask import Flask  # Импортируем класс
 
 app = Flask(__name__)  # Создаем экземпляр класса - наше приложение
 
-@app.route('/')  # URL по которому будет запускаться функция
-def index():
-    return "Index Page"
+@app.route('/user/<username>')  # URL по которому будет запускаться функция
+def show_user_profile(username):
+    return f"User: {username}"
 
-@app.route('/hello')
-def hello():
-    return 'Hello, World!'
+@app.route('/post/<int:post_id>')  # Используется конвертер - показывает какой тип данных принимает функция в качестве аргумента
+def show_post(post_id):
+    return f'Post: {post_id}'
 
 if __name__ == "__main__":
     app.run(debug=True)  # Включается режим отладки !Не использовать в реальных "боевых" проектах
