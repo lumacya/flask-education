d20677a6172f3890d67e9ef419284efb13077672
Ivan Gibert <vngbrt@gmail.com>
2023-02-07 18:09:53 +0300

    added function index

--- main.py
+++ main.py
@@ -2,9 +2,13 @@ from flask import Flask  # Импортируем класс
 
 app = Flask(__name__)  # Создаем экземпляр класса - наше приложение
 
-@app.route("/")  # Декоратор указывающий по какому URL запускать функцию
-def hello_world():
+@app.route('/')  # URL по которому будет запускаться функция
+def index():
+    return "Index Page"
+
+@app.route('/hello')
+def hello():
     return 'Hello, World!'
 
 if __name__ == "__main__":
-    app.run()
+    app.run(debug=True)  # Включается режим отладки !Не использовать в реальных "боевых" проектах
