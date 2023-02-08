9bea4740c0515e2051cd4680c787f3d918787ae3
Ivan Gibert <vngbrt@gmail.com>
2023-02-08 19:42:12 +0300

    edited template and routes

--- app/routes.py
+++ app/routes.py
@@ -5,5 +5,23 @@ from app import app
 @app.route('/')
 @app.route('/index')
 def index():
-    user = {'username': 'Ivan'}
-    return render_template('index.html', title='Home Page',user=user)
+    user = {'username': 'Ivan Gibert'}
+    posts = [
+        {
+            'author': {'username': 'John'},
+            'body': 'Nice weather in Moscow'
+        },
+        {
+            'author': {'username': 'Michael'},
+            'body': 'The Puss in Boots 2 is awesome'
+        },
+        {
+            'author': {'username': 'Ivan'},
+            'body': "I'm studying flask"
+        },
+        {
+            'author': {'username': 'Marcel'},
+            'body': 'On my way to work'
+        }
+    ]
+    return render_template('index.html', title='Home Page',user=user, posts=posts)
